7154fc9a4261a47b95b2bf17434d4094618fd6af
LoganLAVIGNE <dthe01817@gmail.com>
2024-11-23 13:03:15 +0100

    commit

--- src/Command/CacheClearCommand.php
+++ src/Command/CacheClearCommand.php
@@ -1,33 +1,46 @@
 <?php
+// src/Command/CacheClearCommand.php
 
 namespace ApertureLabo\CoreBundle\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(
+    name: 'core:cache-clear',
+    description: 'Clear and warmup the application cache using the umask() function',
+)]
 class CacheClearCommand extends Command
 {
-    protected static $defaultName = 'core:cache-clear';
-
-    protected function configure(): void
+    /* protected function configure(): void
     {
         $this
-            ->setDescription('Clear and warmup application cache with umask() function')
-            ->setHelp('Use this command to clear application cache in production environment');
-    }
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    } */
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $io->text('La commande est fonctionnelle !');
+        /* 
+        $arg1 = $input->getArgument('arg1');
 
-        umask(0022);
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
 
-        $io->writeln('Suppression du cache');
-        exec('php bin/console cache:clear --no-warmup');
-        $io->writeln('Réchauffement du cache');
-        exec('php bin/console cache:warmup --verbose');
-        $io->success('Cache supprimé et réchauffé avec succès');
+        if ($input->getOption('option1')) {
+            $io->note(sprintf('You passed an option: %s', $input->getOption('option1')));
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.'); */
 
         return Command::SUCCESS;
     }
