4dca6e947c7b96c6d12b8cdf715d0ed70ddb18a9
LoganLAVIGNE <dthe01817@gmail.com>
2024-11-23 12:37:36 +0100

    Ajout constantes URL

--- src/Constant/CoreConstants.php
+++ src/Constant/CoreConstants.php
@@ -9,4 +9,14 @@ class CoreConstants {
      */
     public const BUNDLE_NAME = 'core-bundle';
 
+    /**
+     * Fontawesome - 6.4.0 - JS
+     */
+    public const FONTAWESOME_FREE_6_4_0_JS = 'https://www.core.aper-atlas.fr/resources/fontawesome/fontawesome-free-6.4.0-web/js/all.js';
+
+    /**
+     * JQuery - 3.6.4 - JS
+     */
+    public const JQUERY_3_6_4_JS = 'https://www.core.aper-atlas.fr/resources/jquery/jquery-3.6.4.js';
+
 }
